abec07a672346b588d90db32002f0cf33b99b949
skeaven <skeaven@qq.com>
2019-04-09 10:34:23 +0800

    修改构造方法

--- src/main/java/com/skeaven/dbscan/DbscanSpatialEntry.java
+++ src/main/java/com/skeaven/dbscan/DbscanSpatialEntry.java
@@ -9,4 +9,10 @@ import java.util.List;
 @Data
 public class DbscanSpatialEntry extends SpatialEntry {
     private List<DbscanSpatialEntry> subEntries = new LinkedList<>();
+
+    public DbscanSpatialEntry(String id,double x,double y){
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
 }
